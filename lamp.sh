15257a46cfaecc0925f09555dcc6857b8cb2c91e
Tiago <tiagocalado@hotmail.it>
2021-05-30 01:10:14 +0100

    lamp.sh

--- lamp.sh
+++ lamp.sh
@@ -1,4 +1,4 @@
-﻿#!bash
+﻿#!/bin/bash
 sudo apt update -y
 sudo apt upgrade -y
 sudo apt autoremove -y
@@ -20,13 +20,7 @@ if [ "$resp" != "${resp#[Ss]}" ] ;then
  echo exit;
  echo ''
  if [ "$resp" != "${resp#[Ss]}" ] ;then
-    sudo mariadb -u root
-    echo ''
-    echo A criação de password root para a base de dados foi bem sucedida?
-    echo -n "deseja continuar (s/n)? "
-    read resp
-    if [ "$resp" != "${resp#[Ss]}" ] ;then
-      mysql_secure_installation
+      sudo mysql_secure_installation
       sudo apt install php libapache2-mod-php php-mysql -y
       sudo systemctl disable apache2
       sudo systemctl disable mariadb
@@ -35,11 +29,6 @@ if [ "$resp" != "${resp#[Ss]}" ] ;then
       echo ''
       echo Parabéns!, o servidor LAMP foi instalado corretamente.
       echo ''
-    else
-      echo ''
-      echo Apache2 e mariadb possivelmente já estão instalados.
-      echo Verifique a password root da base de dados e volte a correr o script
-    fi
  else
     echo ''
     echo Houve uma tentativa de instalação do apache2 e mariadb-server
@@ -48,4 +37,3 @@ if [ "$resp" != "${resp#[Ss]}" ] ;then
 else 
  echo O sistema foi atualizado, LAMP não foi instalado
 fi
-
